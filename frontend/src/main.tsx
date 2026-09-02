577fc4f2bfa6d7a0b950af618a1ba372f8e4964f
marthanithinkumar-web <marthanithinkumar@gmail.com>
2026-09-02 23:14:29 +0530

    Load compact admin search action styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
+import './styles/admin-service-search.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
